815bd45e1b47e6bf55506ab1ee7256433c600d86
P-HE <ly.he@foxmail.com>
2020-01-05 00:23:47 -0600

    Add: Taking two numbers from the commandline, add them together and output

--- Hello_Word.cpp
+++ Hello_Word.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 
+double a,b;
+
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 	cout << "Hello World" << endl;
-	return 0;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	double c = a + b;
+	cout << c << endl;
+	return 0;	
 }
